3f26f1d08fb3c0817ba8b7b836ddaadde107ed08
Aldo Cahuana <aldooow@icloud.com>
2020-05-27 21:56:51 +0200

    Add version 'WHILE'.

--- js/script.js
+++ js/script.js
@@ -3,21 +3,43 @@
 // Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
 
 // Lista da stampare!!!
-var lista = document.getElementById('js_lista');
+var listaUno = document.getElementById('js_lista-1');
 
 // Stampare una lista di 1 a 100!
-for(var i = 1; i <= 100; i++) {
+for(var i = 1 ; i <= 100; i++) {
   if(i % 15 === 0){
     // ((i % 3 === 0)&&(i % 5 === 0))
     // Per i numeri che sono sia multipli di 3 che di 5 stampi "FizzBuzz".
-    lista.innerHTML += '<li class="fizzbuzz-wrapper">' + 'FizzBuzz' + '</li>';
+    listaUno.innerHTML += '<li class="fizzbuzz-1-wrapper">' + 'FizzBuzz' + '</li>';
   } else if(i % 3 === 0){
     // Per i numeri che sono multipli di 3 stampi "Fizz".
-    lista.innerHTML += '<li class="fizz-wrapper">' + 'Fizz' + '</li>';
+    listaUno.innerHTML += '<li class="fizz-1-wrapper">' + 'Fizz' + '</li>';
   } else if(i % 5 === 0){
     // Per i numeri che sono multipli di 3 stampi "Buzz".
-    lista.innerHTML += '<li class="buzz-wrapper">' + 'Buzz' + '</li>';
+    listaUno.innerHTML += '<li class="buzz-1-wrapper">' + 'Buzz' + '</li>';
   } else {
-    lista.innerHTML += '<li>' + i + '</li>';
+    listaUno.innerHTML += '<li>' + i + '</li>';
   }
+}
+
+
+// VERSIONE WHILE!!!
+var listaDue = document.getElementById('js_lista-2');
+
+var j = 1;
+while( j <= 100 ) {
+  if(j % 15 === 0){
+    // ((i % 3 === 0)&&(i % 5 === 0))
+    // Per i numeri che sono sia multipli di 3 che di 5 stampi "FizzBuzz".
+    listaDue.innerHTML += '<li class="fizzbuzz-2-wrapper">' + 'FizzBuzz' + '</li>';
+  } else if(j % 3 === 0){
+    // Per i numeri che sono multipli di 3 stampi "Fizz".
+    listaDue.innerHTML += '<li class="fizz-2-wrapper">' + 'Fizz' + '</li>';
+  } else if(j % 5 === 0){
+    // Per i numeri che sono multipli di 3 stampi "Buzz".
+    listaDue.innerHTML += '<li class="buzz-2-wrapper">' + 'Buzz' + '</li>';
+  } else {
+    listaDue.innerHTML += '<li>' + j + '</li>';
+  };
+  j++;
 }
